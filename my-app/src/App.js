09e6ba701b0fc4f82e45b6f00b06320d8baa29b5
Yefim Blokh <yefim94@yahoo.com>
2022-05-02 12:58:48 -0400

    added new styles

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,36 +1,37 @@
 import './index.css';
 import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
-
+import image from './assets/business.png'
 function App() {
   const form = useRef();
 
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_dzy220j', 'template_xite6q4', form.current, 'CdolNR6B-_vn2-VUo')
+    emailjs.sendForm('service_yem4m8c', 'template_xite6q4', form.current, 'CdolNR6B-_vn2-VUo')
       .then((result) => {
           console.log(result.text);
       }, (error) => {
           console.log(error.text);
       });
+      alert("email sent")
   };
 
   return (
     <div className="App">
-     <div className='div_one'>
-       <nav>
-         <h1>alumnna</h1>
-         <button><a href='https://expo.dev/@yefim94/alumnna'>LINK TO BETA</a></button>
-       </nav>
-     </div>
      <div className='div_two'>
-       <h2>The task manager for your daily tasks</h2>
-       <img src='https://cdn.discordapp.com/attachments/829914581039579166/967845764497801276/Screen_Shot_2022-04-24_at_1.53.20_PM.png' />
+       <div className='left-side'>
+       <h2>Manage Your Daily Tasks with Alumnna</h2>
+       <p>Manage all your daily, small, stress consuming tasks in one simple app.  With due dates, emojis, and card slots, makes managing tasks quick and effective</p>
+       <button><a href='https://expo.dev/@yefim94/alumnna'>LINK TO BETA</a></button>
+       </div> 
+       <div className='right-side'>
+       <img src={image} width='200' />
+         </div>      
      </div>
      <div className='div_three'>
        <h2>How to download:</h2>
-       <img width='500' src='https://cdn.discordapp.com/attachments/829914581039579166/968625075765018624/Screen_Shot_2022-04-26_at_5.29.15_PM.png' />
+       <img width='200' src='https://cdn.discordapp.com/attachments/829914581039579166/968625075765018624/Screen_Shot_2022-04-26_at_5.29.15_PM.png' />
      </div>
      <div className='div_four'>
        <h2>Email for IOS users:</h2>
